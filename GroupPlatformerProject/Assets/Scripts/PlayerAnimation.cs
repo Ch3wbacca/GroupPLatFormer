bc8f6f1e0fa16bd3bbc53f345c001f889a9e5f55
Preston Ryan <20.preston.ryan@ksd.org>
2018-12-03 13:58:02 -0800

    Animation controler etc

---
+++ GroupPlatformerProject/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Debug.Log(x);
+        float y = Input.GetAxis("Vertical");
+        if (Mathf.Abs(x) > 0)
+        {
+            GetComponent<Animator>().SetFloat("x", x);
+
+        }
+        if (x == 0)
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+
+    }
+}
