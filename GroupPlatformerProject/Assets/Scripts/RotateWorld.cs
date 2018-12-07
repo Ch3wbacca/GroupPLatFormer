fff83a3ae05c83c993244df9675f3c2708c2ea2e
Maxwell Dickes <20.maxwell.dickes@ksd.org>
2018-12-07 13:13:49 -0800

    first working rotate

--- GroupPlatformerProject/Assets/Scripts/RotateWorld.cs
+++ GroupPlatformerProject/Assets/Scripts/RotateWorld.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RotateWorld : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class RotateWorld : MonoBehaviour {
     private Quaternion targetRotation;
     public bool rotateMe = false;
     float degrees = 0;
+    public GameObject wall1;
+    public GameObject wall2;
     Vector3 speed;
     // Use this for initialization
     void Start () {
@@ -28,7 +31,7 @@ public class RotateWorld : MonoBehaviour {
             speed = GetComponent<Rigidbody2D>().velocity;
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
         }
-        else
+        else if (rotateMe && degrees >= 180)
         {
             rotateMe = false;
             degrees = 0;
@@ -43,9 +46,22 @@ public class RotateWorld : MonoBehaviour {
         {
             //Time.timeScale = 0;
             rotateMe = true;
+            wall1.SetActive(true);
+            wall2.SetActive(true);
             //targetRotation *= Quaternion.AngleAxis(180, new Vector3(0,0,1));
             //RotateCamera.transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
             //RotateCamera.transform.Rotate(0, 0, rotateSpeed, Space.World);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wall1")
+        {
+            SceneManager.LoadScene("UpsideDown1");
+        }
+        if (collision.gameObject.name == "Wall2")
+        {
+            SceneManager.LoadScene("UpsideDown1");
         }
     }
 }
