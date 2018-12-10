8db3f3998d721234e1812f09d05bee6f89a6616e
Maxwell Dickes <20.maxwell.dickes@ksd.org>
2018-12-10 13:01:22 -0800

    cows in but not anim

---
+++ GroupPlatformerProject/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+    public GameObject camera;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "CameraZoomOut")
+        {
+            //i want to make the transform postion Y to be equal to -2.5
+        }
+    }
+}
