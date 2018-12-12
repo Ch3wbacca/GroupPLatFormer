4a7e7a26e94f6008bd2aeae5f7ade206cf122606
Maxwell Dickes <20.maxwell.dickes@ksd.org>
2018-12-12 12:39:06 -0800

    random all over

---
+++ GroupPlatformerProject/Assets/Scripts/LoadLevel1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
