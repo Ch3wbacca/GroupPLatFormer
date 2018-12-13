9f5121673fd19f1e66a35ddb3c0cac0be17e6644
Maxwell Dickes <20.maxwell.dickes@ksd.org>
2018-12-13 12:55:39 -0800

    to fix cow anim

---
+++ GroupPlatformerProject/Assets/Scripts/WinSceneLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinSceneLoad : MonoBehaviour {
+
+    public GameObject BG1;
+    public GameObject BG2;
+    public GameObject BG3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
