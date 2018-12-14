7222b9d2a4b9c513624a3793d489e3385951e1ce
Preston Ryan <20.preston.ryan@ksd.org>
2018-12-14 12:52:36 -0800

    worked in win scene

---
+++ GroupPlatformerProject/Assets/Scripts/WinButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WinButtons : MonoBehaviour
+{
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+
+}
